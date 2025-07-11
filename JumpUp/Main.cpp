c720e5927815609316e4c22f7b97d59d2000ee7b
ryota37 <ryota.nakahama@access-company.com>
2025-07-11 11:24:06 +0900

    Initial commit

--- JumpUp/Main.cpp
+++ JumpUp/Main.cpp
@@ -1,10 +1,26 @@
 ﻿# include <Siv3D.hpp> // Siv3D v0.6.15
 
+
+
+
+
 void Main()
 {
+	Window::Resize(600, 800);
+
+	Circle player(300, 700, 20);
 
 	while (System::Update())
 	{
+
+		player.draw(Palette::Blue);
+
+		if (KeyLeft.pressed()) player.moveBy(-5, 0);
+		if (KeyRight.pressed()) player.moveBy(5, 0);
+
+		if (KeySpace.pressed()) player.moveBy(0, -5);
+
+
 	}
 }
 
