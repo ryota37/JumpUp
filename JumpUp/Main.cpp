1b8bf188fa64d909433f6c3ddc60f1df7bfa49d1
ryota37 <ryota.nakahama@access-company.com>
2025-07-12 19:36:43 +0900

    Impl: (WIP) scaffold

--- JumpUp/Main.cpp
+++ JumpUp/Main.cpp
@@ -1,14 +1,30 @@
 ﻿# include <Siv3D.hpp> // Siv3D v0.6.15
 
+class Scaffold
+{
+private:
+	Rect m_rect;
+public:
+	Scaffold(int x, int y, int width, int height)
+		: m_rect(x, y, width, height) {
+	}
 
-
-
+	void draw() const
+	{
+		m_rect.draw(Palette::Green);
+	}
+	bool isColliding(const Circle& player) const
+	{
+		return m_rect.intersects(player);
+	}
+};
 
 void Main()
 {
 	Window::Resize(600, 800);
 
 	Circle player(300, 700, 20);
+	Scaffold scaffold(0, 750, 600, 50);
 
 	while (System::Update())
 	{
@@ -19,6 +35,8 @@ void Main()
 		if (KeyRight.pressed()) player.moveBy(5, 0);
 
 		if (KeySpace.pressed()) player.moveBy(0, -5);
+
+		scaffold.draw();
 
 
 	}
